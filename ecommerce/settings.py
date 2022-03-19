73f759f9378d3750e3e8a2cbc54ffcd0e827bf02
azamatnarzuloyev <94824948+azamatnarzuloyev@users.noreply.github.com>
2022-03-19 14:54:15 +0500

    push

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -168,7 +168,7 @@ REST_FRAMEWORK = {
 
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 24
+    'PAGE_SIZE': 5
     # 'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 
