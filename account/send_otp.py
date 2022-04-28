13b38cfae07a4ea807ae55bb1db4e317cea7ab02
azamatnarzuloyev <94824948+azamatnarzuloyev@users.noreply.github.com>
2022-04-28 09:09:02 +0500

    sms otp

--- account/send_otp.py
+++ account/send_otp.py
@@ -1,11 +1,13 @@
 from telnetlib import SEND_URL
 from django.core.cache import cache
 from django.conf import settings
-
+import http.client
+import mimetypes
+from codecs import encode
 from rest_framework.response import Response
 from rest_framework import status
-import requests
 from extensions.code_generator import otp_generator
+import requests
 
 
 # send otp code 
@@ -14,27 +16,24 @@ def send_otp(*, user_otp: object, phone: str):
     otp = otp_generator()
     user_otp.otp = otp
     cache.set(phone, otp, settings.EXPIRY_TIME_OTP)
-    external_api_url = f'https://api.telegram.org/bot5245455385:AAGIxYYiHeul7EqguviPZtprUbzu_Te-Zh4/sendMessage?chat_id=-1001638264258&parse_mode=html&text="{otp}"'
-    res = requests.post(external_api_url)
-   
-    # link = f'https://2factor.in/API/R1/?module=TRANS_SMS&apikey=fc9e5177-b3e7-11e8-a895-0200cd936042&to={phone}&from=wisfrg&templatename=wisfrags&var1={otp_key}'
-   
-    # result = requests.get(link, verify=False)
+    # external_api_url = f'https://api.telegram.org/bot5245455385:AAGIxYYiHeul7EqguviPZtprUbzu_Te-Zh4/sendMessage?chat_id=-1001638264258&parse_mode=html&text="{otp}"'
+    # res = requests.post(external_api_url)    
+    url = f'http://notify.eskiz.uz/api/message/sms/send?mobile_phone={phone}&from=4546&message={otp}' 
+    conn = http.client.HTTPSConnection("notify.eskiz.uz")
+    files = []
+    headers= {'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjgyOCwicm9sZSI6InVzZXIiLCJkYXRhIjp7ImlkIjo4MjgsIm5hbWUiOiJPT08gU01BUlQgVEVDSE5PTE9HWSBTWVNURU1TIiwiZW1haWwiOiJhemFtYXRzYWJpbmExNzk2QG1haWwucnUiLCJyb2xlIjoidXNlciIsImFwaV90b2tlbiI6bnVsbCwic3RhdHVzIjoiYWN0aXZlIiwic21zX2FwaV9sb2dpbiI6ImVza2l6MiIsInNtc19hcGlfcGFzc3dvcmQiOiJlJCRrIXoiLCJ1el9wcmljZSI6NTAsImJhbGFuY2UiOjMwMDAwMCwiaXNfdmlwIjowLCJob3N0Ijoic2VydmVyMSIsImNyZWF0ZWRfYXQiOiIyMDIyLTA0LTI3VDA1OjI1OjM1LjAwMDAwMFoiLCJ1cGRhdGVkX2F0IjoiMjAyMi0wNC0yN1QwNToyNTo1NC4wMDAwMDBaIn0sImlhdCI6MTY1MTA1Nzk5NiwiZXhwIjoxNjUzNjQ5OTk2fQ.C3Tvo6wG8-pW1IB3UharPxWemOOhRSINHb_raGBVteg'}
 
-    # return otp
-    # Here the otp code must later be sent to the user's phone number by SMS system.
-    print(f"your phone: {phone}")
-    print(f"your otp code :: {otp} ")
-    # But in debug mode we print the otp code.
+
+    response = requests.request("POST", url, headers=headers,  files=files)
+    user_otp.count+=1
+    user_otp.save(update_fields=['otp','count'])
+    print(response.text)
     
-    user_otp.count += 1
-    user_otp.save(update_fields=["otp", "count"])
-    context = {
-        "code sent.": "The code has been sent to the desired phone number.",
-    }
     return Response(
  
-        res.json(),
+        response.json(),
         status=status.HTTP_200_OK,
     )
-    # return Response(context,res.json())
+    print(response.text)
+  
+    print(otp)
