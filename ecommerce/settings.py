cae11af4c9ff13103db9325e62a5c842f475dab1
azamatnarzuloyev <94824948+azamatnarzuloyev@users.noreply.github.com>
2022-05-11 09:15:18 +0500

    push

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =env.bool('DEBUG', default=False)
-# DEBUG =True
+# DEBUG =env.bool('DEBUG', default=False)
+DEBUG =False
 
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
