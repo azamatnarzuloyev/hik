1576c920c418fea04d4f4173df5719e3c7a1a6d5
azamatnarzuloyev <94824948+azamatnarzuloyev@users.noreply.github.com>
2022-05-14 14:36:04 +0500

    Update settings.py
    commiyt

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG =env.bool('DEBUG', default=False)
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['138.68.108.160','127.0.0.1']
 
@@ -183,7 +183,7 @@ from datetime import timedelta
 STATIC_URL = '/static/'
 STATIC_ROOT = str(BASE_DIR.joinpath('static'))
 MEDIA_URL = "/media/"
-# MEDIA_ROOT = "media"
+
 
 # MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
