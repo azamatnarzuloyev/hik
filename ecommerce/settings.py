febc054b35d68934679861e0d2312c3dc04caa5f
azamatnarzuloyev <94824948+azamatnarzuloyev@users.noreply.github.com>
2022-05-14 12:47:37 +0500

    Update settings.py
    dugug update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG =env.bool('DEBUG', default=False)
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['64.227.123.156','127.0.0.1']
 
