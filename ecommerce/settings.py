acbad88b3218093b8840497ce7ffae57bfe79731
azamatnarzuloyev <94824948+azamatnarzuloyev@users.noreply.github.com>
2022-05-14 15:31:54 +0500

    Update settings.py
    pull request

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG =env.bool('DEBUG', default=False)
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['138.68.108.160','127.0.0.1']
 
