1c94a314364821a1aecd57df880d7747bb0e251b
root <you@example.com>
2022-05-15 10:19:29 +0000

    push

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG =env.bool('DEBUG', default=False)
 
-DEBUG = False
+DEBUG =False
 
 ALLOWED_HOSTS = ['64.227.123.156','127.0.0.1']
 
@@ -184,7 +184,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = str(BASE_DIR.joinpath('static'))
 MEDIA_URL = "/media/"
 # MEDIA_ROOT = "media"
-
 # MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
