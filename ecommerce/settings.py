8e31d35837673683569115abf56148b56d687f91
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2022-05-24 15:07:04 +0500

    Update settings.py
    pegination update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -228,7 +228,7 @@ REST_FRAMEWORK = {
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
     # # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 12
 }
 
 
