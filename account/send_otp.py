f821b8cf657a4dddacb41c1b204a18b5dd0d174b
azamatnarzulloyev <azamatsabina1796@mail.ru>
2022-05-25 18:20:30 +0500

    push

--- account/send_otp.py
+++ account/send_otp.py
@@ -1,9 +1,7 @@
-from telnetlib import SEND_URL
 from django.core.cache import cache
 from django.conf import settings
 import http.client
 import mimetypes
-from codecs import encode
 from rest_framework.response import Response
 from rest_framework import status
 from extensions.code_generator import otp_generator
