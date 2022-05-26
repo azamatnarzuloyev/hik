f0b5955286d3531cbdac551c447d5f69437113aa
azamatnarzulloyev <azamatsabina1796@mail.ru>
2022-05-26 17:53:29 +0500

    push

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -182,7 +182,7 @@ USE_L10N=True
 USE_TZ = True
 
 AUTH_USER_MODEL = 'account.User'
-SITE_ID=1
+# SITE_ID=1
 from datetime import timedelta
 
 
@@ -240,8 +240,8 @@ REST_FRAMEWORK = {
     # # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_PAGINATION_CLASS': 'ecommerce.utils.custom_pagination.CustomPagination',
     'PAGE_SIZE': 12,
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    # 'PAGE_SIZE': 12
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 12
 }
 
 
