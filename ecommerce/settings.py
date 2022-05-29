940911c19eb932f129a4b58f2a8d95d98d9c27ee
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2022-05-30 03:21:33 +0500

    Update settings.py
    fdfdf

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -172,8 +172,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Tashkent' 
-# TIME_ZONE = 'UTC'
+# TIME_ZONE = 'Asia/Tashkent' 
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
