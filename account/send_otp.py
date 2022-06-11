178f1624ffabd8ed36f7cb20c14c585053dce1c5
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2022-06-11 16:53:16 +0500

    Update send_otp.py
    dsdsds

--- account/send_otp.py
+++ account/send_otp.py
@@ -16,7 +16,7 @@ def send_otp(*, user_otp: object, phone: str):
     cache.set(phone, otp, settings.EXPIRY_TIME_OTP)
     # external_api_url = f'https://api.telegram.org/bot5245455385:AAGIxYYiHeul7EqguviPZtprUbzu_Te-Zh4/sendMessage?chat_id=-1001638264258&parse_mode=html&text="{otp}"'
     # res = requests.post(external_api_url)    
-    url = f'http://notify.eskiz.uz/api/message/sms/send?mobile_phone={phone}&from=4546&message=Hikvision_kod:{otp}' 
+    url = f'http://notify.eskiz.uz/api/message/sms/send?mobile_phone={phone}&from=4546&message=sts-hik.uz kirish uchun parol:{otp}' 
     conn = http.client.HTTPSConnection("notify.eskiz.uz")
     files = []
     headers= {'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjgyOCwicm9sZSI6InVzZXIiLCJkYXRhIjp7ImlkIjo4MjgsIm5hbWUiOiJPT08gU01BUlQgVEVDSE5PTE9HWSBTWVNURU1TIiwiZW1haWwiOiJhemFtYXRzYWJpbmExNzk2QG1haWwucnUiLCJyb2xlIjoidXNlciIsImFwaV90b2tlbiI6bnVsbCwic3RhdHVzIjoiYWN0aXZlIiwic21zX2FwaV9sb2dpbiI6ImVza2l6MiIsInNtc19hcGlfcGFzc3dvcmQiOiJlJCRrIXoiLCJ1el9wcmljZSI6NTAsImJhbGFuY2UiOjI4ODMwMCwiaXNfdmlwIjowLCJob3N0Ijoic2VydmVyMSIsImNyZWF0ZWRfYXQiOiIyMDIyLTA0LTI3VDA1OjI1OjM1LjAwMDAwMFoiLCJ1cGRhdGVkX2F0IjoiMjAyMi0wNS0yN1QxOTo0NToxOC4wMDAwMDBaIn0sImlhdCI6MTY1Mzg2MjE4MywiZXhwIjoxNjU2NDU0MTgzfQ.mv0Lk7s5k2OzpjZIno90mldHvcCzHspgwKYOAPyjTEU'}
