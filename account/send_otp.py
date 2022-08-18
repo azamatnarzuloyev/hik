7d610851d3cafe815bb9520b752a12cbcde14292
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2022-08-18 12:46:57 +0500

    Update send_otp.py
    sasasasas

--- account/send_otp.py
+++ account/send_otp.py
@@ -1,16 +1,23 @@
 
-import http.client
-import requests
-from extensions.code_generator import otp_generator
-from rest_framework import status
-from rest_framework.response import Response
+from django.core.cache import cache
 from django.conf import settings
+import http.client
+import mimetypes
+from rest_framework.response import Response
+from rest_framework import status
+from extensions.code_generator import otp_generator
+import requests
+
 
 # send otp code 
 
 def send_otp(*, user_otp: object, phone: str):
     otp = otp_generator()
-    user_otp.otp = otp  
+    user_otp.otp = otp
+    cache.set(phone, settings.EXPIRY_TIME_OTP)
+    cache.set(phone, otp, settings.EXPIRY_TIME_OTP)
+    # external_api_url = f'https://api.telegram.org/bot5245455385:AAGIxYYiHeul7EqguviPZtprUbzu_Te-Zh4/sendMessage?chat_id=-1001638264258&parse_mode=html&text="{otp}"'
+    # res = requests.post(external_api_url)    
     url = f'http://notify.eskiz.uz/api/message/sms/send?mobile_phone={phone}&from=4546&message=sts-hik.uz kirish uchun parol:{otp}' 
     conn = http.client.HTTPSConnection("notify.eskiz.uz")
     files = []
@@ -19,11 +26,9 @@ def send_otp(*, user_otp: object, phone: str):
 
     response = requests.request("POST", url, headers=headers,  files=files)
     user_otp.count+=1
-    user_otp.save(update_fields=['otp','count'])
-
-    
+    user_otp.save(update_fields=['otp','count']) 
+    # print(response.text)
     return Response(
- 
         response.json(),
         status=status.HTTP_200_OK,
     )
