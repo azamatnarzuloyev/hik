953e4bd12f7fc778ffc65d8f8b7b130a8f3fd5ab
Azamat <94824948+azamatnarzuloyev@users.noreply.github.com>
2022-08-21 00:46:35 +0500

    Update send_otp.py
    zxzxzzxzxzxxxzzx

--- account/send_otp.py
+++ account/send_otp.py
@@ -1,28 +1,24 @@
-
 import requests
 from django.core.cache import cache
-from extensions.code_generator import otp_generator
+from extensions.code_generator import otp_generator, get_client_ip
 from rest_framework import status
 from rest_framework.response import Response
 from django.conf import settings
 
 # send otp code 
-def send_otp(*, user_otp: object, phone: str):
+def send_otp(request, phone):
     otp = otp_generator()
-    user_otp.otp = otp
-    cache.set(phone, settings.EXPIRY_TIME_OTP)
+    ip = get_client_ip(request)
+    # user_otp.otp = otp
+    cache.set(f"{ip}-for-authentication", phone, settings.EXPIRY_TIME_OTP)
     cache.set(phone, otp, settings.EXPIRY_TIME_OTP)
+    # user_otp.otp = otp
 
-    url = f'http://notify.eskiz.uz/api/message/sms/send?mobile_phone={phone}&from=4546&message=sts-hik.uz kirish uchun parol:{otp}'
+    url = f'http://notify.eskiz.uz/api/message/sms/send?mobile_phone={phone}&from=4546&message=sts-hik.uz:kirish uchun parol {otp}'
     headers = {'Authorization': f'Bearer {settings.SMS_TOKEN}'}
 
 
     response = requests.post(url, headers=headers)
-    if response.status_code == 200:
-        user_otp.count+=1
-        user_otp.save(update_fields=['otp','count'])
-
-
     return Response(
         response.json(),
         status=status.HTTP_200_OK,
